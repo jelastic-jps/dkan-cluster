769ded6eaec60077483870b00cebe465e2103c3c
HENADII SYCHEVSKYI <SGI@HENADIIs-MacBook-Pro-4.local>
2020-06-03 14:42:19 +0300

    Increase disk limit

--- scripts/beforeinstall.js
+++ scripts/beforeinstall.js
@@ -18,7 +18,7 @@ resp.nodes.push({
   count: 2,
   flexibleCloudlets: ${settings.db_flexibleCloudlets:16},
   fixedCloudlets: ${settings.db_fixedCloudlets:1},
-  diskLimit: ${settings.db_diskLimit:10},
+  diskLimit: ${settings.db_diskLimit:100},
   nodeGroup: "sqldb",
   skipNodeEmails: true,
   displayName: "DB Server"
@@ -41,7 +41,7 @@ resp.nodes.push({
   count: 1,
   flexibleCloudlets: ${settings.bl_flexibleCloudlets:8},
   fixedCloudlets: ${settings.bl_fixedCloudlets:1},
-  diskLimit: ${settings.bl_diskLimit:10},
+  diskLimit: ${settings.bl_diskLimit:100},
   nodeGroup: "bl",
   scalingMode: "STATEFUL",
   displayName: "Load balancer"
@@ -51,7 +51,7 @@ resp.nodes.push({
   count: ${settings.cp_count:2},
   flexibleCloudlets: ${settings.cp_flexibleCloudlets:8},                  
   fixedCloudlets: ${settings.cp_fixedCloudlets:1},
-  diskLimit: ${settings.cp_diskLimit:10},
+  diskLimit: ${settings.cp_diskLimit:100},
   nodeGroup: "cp",
   scalingMode: "STATELESS",
   displayName: "AppServer",
